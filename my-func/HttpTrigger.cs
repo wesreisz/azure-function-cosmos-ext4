c68204980c8075de4a5b43c56da43f8940cbf977
Wesley Reisz <wes@wesleyreisz.com>
2023-02-05 13:38:00 -0500

    Stuck on Connection String to Mongo

--- my-func/HttpTrigger.cs
+++ my-func/HttpTrigger.cs
@@ -8,16 +8,21 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
+//TODO: Unable to pass in the cosmosdb connection string into this code. It fails everytime.
+//The first issue was using 4.0.0 I dropped it down to 3.0.10 and it runs but I get a null
+//for the connection string. Stuck here until this is solved.
+
 namespace com.wesleyreisz.example
 {
     public static class HttpTrigger
     {
         [FunctionName("HttpTrigger")]
         public static async Task<IActionResult> Run(
-            [CosmosDB(
-                databaseName: "my-database", 
-                containerName: "my-container")]IAsyncCollector<dynamic> documentsOut,
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            // [CosmosDB(
+            //     databaseName: "my-database",
+            //     collectionName: "my-container",
+            //     ConnectionStringSetting = "CosmosDbConnectionString")]IAsyncCollector<dynamic> documentsOut,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
@@ -27,6 +32,17 @@ namespace com.wesleyreisz.example
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
+
+            // if (!string.IsNullOrEmpty(name))
+            // {
+            //     // Add a JSON document to the output container.
+            //     await documentsOut.AddAsync(new
+            //     {
+            //         // create a random ID
+            //         id = System.Guid.NewGuid().ToString(),
+            //         name = name
+            //     });
+            // }
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
